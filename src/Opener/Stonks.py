f75c93916b1259af5898bd3777a90c51d80b4dd1
vigneshkarthikeyan <vignesh@alumni.purdue.edu>
2020-09-01 23:24:52 +0530

    stonks

---
+++ src/Opener/Stonks.py
@@ -0,0 +1,8 @@
+# Three lists of prices of length n: L, H, and S, max profit you could have made from a sale range, buy and sell same-day
+# Setup:
+n = 5
+L = [3, 5, 7, 9, 1]
+H = [4, 6, 8, 10, 2]
+
+# naive
+for i in range(n):
